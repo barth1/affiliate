019b0e4c081085174ed7214bbe2e6b6f2e6be2db
barth bassey <basseybartholomew237@gmail.com>
2021-03-23 14:41:14 +0100

    third commit

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
